36169322d4fd16140560e0abdae3dd362f0c912d
Joshua Day <lovesmath314@gmail.com>
2018-12-22 02:54:20 -0600

    fixed broken links to chapters

--- chapters/chapter01/play-2-1-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -s "https://cs-education.github.io/sysassets/chapters/chapter01/play-2-1-test.c" | gcc -xc - -o test
-if [ $? != 0 ]
-then
-    echo compilation failed
-else
-    ./test ./program
-fi
--- chapters/chapter02/play-1-1-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -s "https://cs-education.github.io/sysassets/chapters/chapter02/play-1-1-test.c" | gcc -xc - -o test
-if [ $? != 0 ]
-then
-    echo compilation failed
-else
-    ./test ./program
-fi
